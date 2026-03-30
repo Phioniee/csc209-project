c74c8e299395b53c9b31626f1d4a503569544b38
Sara <sarakopilovic@gmail.com>
2026-03-30 21:09:54 +0000

    manager.h updates

--- process_manager.h
+++ process_manager.h
@@ -10,11 +10,17 @@ typedef struct {
         int from_worker[2];     //worker writes and parent reads
 } WorkerInformation;
 
+//worker lifecycle
 void create_workers(WorkerInformation workers[], int num_workers);
+void wait_for_workers(WorkerInformation workers[], int num_workers);
+
+//communication
 void send_task(int worker_fd, const char *filename);
 void send_terminate(int worker_fd);
 void receive_results(int worker_fd, WordCount *results, int *size);
+
+//cleanup
 void close_parent_pipes(WorkerInformation workers[], int num_workers);
-void wait_for_workers(WorkerInformation workers[], int num_workers);
+
 
 #endif
